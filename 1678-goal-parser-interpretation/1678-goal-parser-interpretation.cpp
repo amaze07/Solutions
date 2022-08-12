0264344598970fc5c27fbed22cc022b1fc3d674d
amaze07 <89767281+amaze07@users.noreply.github.com>
2022-08-12 09:18:57 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (55.90%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string str = "";
+        for(int i = 0; i < command.size(); i++){
+            if(command[i] == 'G') str += 'G';
+            else if(command[i] == '(' && command[i+1] == ')') str += 'o';
+            else if(command[i] == '(' && command[i+1] == 'a') str += "al";
+            else continue;
+        }
+        return str;
+    }
+};
